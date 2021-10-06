10bfd21c2ebcc9ebd3ee3b8b5e95f0c3972d7106
nirrozdata <87060389+nirrozdata@users.noreply.github.com>
2021-10-06 15:26:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import dtlpy as dl
 import logging
-import time
+
+logger = logging.getLogger(name=__name__)
 
 class ServiceRunner(dl.BaseServiceRunner):
 
-    def update_item_metadata(self, item):
-        # Get Item - update metadata
+    def move_item(self, item):
+        # Get dataset
+        dataset = item.dataset
 
-        item.metadata["user"] = {"user" : time.time()}
-        item.update()
-        
+        dataset.items.make_dir(directory="/Faas")
+        dst_folder = '/Faas'
+        item.move(new_path=dst_folder)
 
         print('Successfully move all items to new dir')
-
-
+        
